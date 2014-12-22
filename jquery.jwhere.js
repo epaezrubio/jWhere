d18a29c484b05b9d9ead80117972f9412d79d9ef
Eduardo Paez <epaezrubio@gmail.com>
2014-12-22 14:05:16 +0100

    Similar test suites grouped

--- jquery.jwhere.js
+++ jquery.jwhere.js
@@ -9,142 +9,101 @@
 
     "use strict";
 
-    var filterSuites = {
-        "html": {
-            testString: function ($element, string) {
-                return $element.html() === string;
+    var fnGroupsObjects = {
+            numberFunctionObject: function (testProp) {
+                return {
+                    testNumber: function ($element, number) {
+                        return $element[testProp]() === number;
+                    },
+                    testFunction: function ($element, fn) {
+                        return fn($element[testProp]())
+                    }
+                }
             },
-            testRegexp: function ($element, regexp) {
-                return $element.html().match(regexp);
+            stringRegexpFunctionObject: function(testProp) {
+                return {
+                    testString: function ($element, string) {
+                        return $element[testProp]() === string;
+                    },
+                    testRegexp: function ($element, regexp) {
+                        return $element[testProp]().match(regexp);
+                    },
+                    testFunction: function ($element, fn) {
+                        return fn($element[testProp]())
+                    }
+                }
             },
-            testFunction: function ($element, fn) {
-                return fn($element.html())
+            keyValuekeyRegexpKeyFn: function (testProp) {
+                return {
+                    testKeyValue: function ($element, object) {
+                        return $element[testProp](object.key) == object.value;
+                    },
+                    testKeyRegexp: function ($element, object) {
+                        return ("" + $element[testProp](object.key)).match(object.value)
+                    },
+                    testKeyValueFn: function ($element, object) {
+                        return object.value($element[testProp](object.key));
+                    }
+                }
             }
         },
-        "text": {
-            testString: function ($element, string) {
-                return $element.text() === string;
-            },
-            testRegexp: function ($element, regexp) {
-                return $element.text().match(regexp);
-            },
-            testFunction: function ($element, fn) {
-                return fn($element.text())
+        filterSuites = {
+            "html": fnGroupsObjects.stringRegexpFunctionObject("html"),
+            "text": fnGroupsObjects.stringRegexpFunctionObject("text"),
+            "width": fnGroupsObjects.numberFunctionObject("width"),
+            "height": fnGroupsObjects.numberFunctionObject("height"),
+            "outerHeight": fnGroupsObjects.numberFunctionObject("outerHeight"),
+            "innerHeight": fnGroupsObjects.numberFunctionObject("innerHeight"),
+            "outerWidth": fnGroupsObjects.numberFunctionObject("outerWidth"),
+            "innerWidth": fnGroupsObjects.numberFunctionObject("innerWidth"),
+            "css": fnGroupsObjects.keyValuekeyRegexpKeyFn("css"),
+            "data": fnGroupsObjects.keyValuekeyRegexpKeyFn("data"),
+            "children": {
+                testNumber: function ($element, number) {
+                    return $element.children().length === number;
+                },
+                testFunction: function ($element, fn) {
+                    return fn($element.children())
+                }
             }
-        },
-        "css": {
-            testKeyValue: function ($element, object) {
-                return $element.css(object.key) == object.value;
-            },
-            testKeyValueFn: function ($element, object) {
-                return object.value($element.css(object.key));
-            }
-        },
-        "data": {
-            testKeyValue: function ($element, object) {
-                return $element.data(object.key) == object.value;
-            },
-            testKeyRegexp: function ($element, object) {
-                return ("" + $element.data(object.key)).match(object.value)
-            },
-            testKeyValueFn: function ($element, object) {
-                return object.value($element.data(object.key));
-            }
-        },
-        "width": {
-            testNumber: function ($element, number) {
-                return $element.width() === number;
-            },
-            testFunction: function ($element, fn) {
-                return fn($element.width())
-            }
-        },
-        "height": {
-            testNumber: function ($element, number) {
-                return $element.height() === number;
-            },
-            testFunction: function ($element, fn) {
-                return fn($element.height())
-            }
-        },
-        "outerHeight": {
-            testNumber: function ($element, number) {
-                return $element.outerHeight() === number;
-            },
-            testFunction: function ($element, fn) {
-                return fn($element.innerHeight())
-            }
-        },
-        "innerHeight": {
-            testNumber: function ($element, number) {
-                return $element.innerHeight() === number;
-            },
-            testFunction: function ($element, fn) {
-                return fn($element.innerHeight())
-            }
-        },
-        "outerWidth": {
-            testNumber: function ($element, number) {
-                return $element.outerWidth() === number;
-            },
-            testFunction: function ($element, fn) {
-                return fn($element.innerWidth())
-            }
-        },
-        "innerWidth": {
-            testNumber: function ($element, number) {
-                return $element.innerWidth() === number;
-            },
-            testFunction: function ($element, fn) {
-                return fn($element.innerWidth())
-            }
-        },
-        "children": {
-            testNumber: function ($element, number) {
-                return $element.children().length === number;
-            },
-            testFunction: function ($element, fn) {
-                return fn($element.children())
-            }
-        }
-    }, getAssertionFunction = function (key, assertion) {
+        }, getAssertionFunction = function (key, assertion) {
 
-        if (!key || !filterSuites[key]) {
+            if (!key || !filterSuites[key]) {
+                return null;
+            }
+
+            if ((assertion === "" + assertion) && assertion.indexOf) {
+                return filterSuites[key].testString;
+            }
+
+            if (assertion + 0 === assertion) {
+                return filterSuites[key].testNumber;
+            }
+
+            if (assertion.compile) {
+                return filterSuites[key].testRegexp;
+            }
+
+            if (assertion.prototype && assertion.prototype.constructor) {
+                return filterSuites[key].testFunction;
+            }
+
+            if (assertion.key && assertion.value) {
+
+                if (assertion.value.prototype && assertion.value.prototype.constructor) {
+                    return filterSuites[key].testKeyValueFn;
+                }
+
+                if (assertion.value.compile) {
+                    return filterSuites[key].testKeyRegexp;
+                }
+
+                return filterSuites[key].testKeyValue;
+            }
+
             return null;
-        }
 
-        if ((assertion === "" + assertion) && assertion.indexOf) {
-            return filterSuites[key].testString;
-        }
-
-        if (assertion + 0 === assertion) {
-            return filterSuites[key].testNumber;
-        }
-
-        if (assertion.compile) {
-            return filterSuites[key].testRegexp;
-        }
-
-        if (assertion.prototype && assertion.prototype.constructor) {
-            return filterSuites[key].testFunction;
-        }
-
-        if (assertion.key && assertion.value) {
-
-            if (assertion.value.prototype && assertion.value.prototype.constructor) {
-                return filterSuites[key].testKeyValueFn;
-            }
-
-            if (assertion.value.compile) {
-                return filterSuites[key].testKeyRegexp;
-            }
-
-            return filterSuites[key].testKeyValue;
-        }
-
-        return null;
-
-    };
+        };
 
     $.fn.where = function (filters) {
 
